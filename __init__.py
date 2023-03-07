779190f3f1c001d968695cdbb9622e0872568031
daniel-life <Daniel08222003@gmail.com>
2023-03-07 16:58:24 +0800

    added pages and train system map

--- __init__.py
+++ __init__.py
@@ -10,5 +10,21 @@ def home():
 def contacts():
      return render_template('contacts.html')
 
+@app.route('/train_system')
+def train_system():
+     return render_template('train_system.html')
+
+@app.route('/train_guide')
+def train_guide():
+     return render_template('train_guide.html')
+
+@app.route('/bus_services')
+def bus_services():
+     return render_template('bus_services.html')
+
+@app.route('/bus_guide')
+def bus_guide():
+     return render_template('bus_guide.html')
+
 if __name__ == '__main__':
     app.run()
